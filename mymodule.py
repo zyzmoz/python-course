55bac3b2e3823a354b449b4f06eb5d0e058a2db1
Daniel Cunha <danielbrandaodacunha@gmail.com>
2020-03-08 16:03:05 -0300

    Packages and modules

---
+++ mymodule.py
@@ -0,0 +1,2 @@
+def func_my_module():
+  print("I'm in my module")
---
+++ myprogram.py
@@ -0,0 +1,8 @@
+import mymodule
+import mymodule as mm
+from mymodule import func_my_module
+
+mymodule.func_my_module()
+mm.func_my_module()
+
+func_my_module()
